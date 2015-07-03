9d533e5301929115943c2c30b9919178ad1e8cc9
dynabit <dynabit@users.noreply.github.com>
2015-07-03 15:50:35 +0800

    KVS add convert from T to AS Key/Bins with a offset of Long type

--- test/java/com/silvrr/framework/kvs/TestKVS.java
+++ test/java/com/silvrr/framework/kvs/TestKVS.java
@@ -4,11 +4,15 @@ import com.aerospike.client.Bin;
 import com.aerospike.client.Key;
 import com.aerospike.client.async.AsyncClient;
 import com.aerospike.client.policy.CommitLevel;
+import com.aerospike.client.policy.ScanPolicy;
 import com.aerospike.client.policy.WritePolicy;
 import com.silvrr.framework.OperateRequest;
+import com.silvrr.framework.kvs.ASDObjectConverter.BinsOf;
+import com.silvrr.framework.kvs.ASDObjectConverter.KeyOf;
 
 import static com.silvrr.framework.kvs.AerospikeCallback.SeperateWriteCallback.*;
 import static com.silvrr.framework.kvs.AerospikeCallback.SingleWriteCallback.*;
+import static com.silvrr.framework.kvs.ASDObjectConverter.*;
 
 public class TestKVS {
 	
@@ -18,20 +22,30 @@ public class TestKVS {
 	}
 	
 	private AsyncClient client = new AsyncClient("localhost", 3000);
-	private WritePolicy policy = new WritePolicy();
+	private WritePolicy wPolicy = new WritePolicy();
+	private ScanPolicy sPolicy = new ScanPolicy();
 	
-	private TestKVS(){policy.commitLevel=CommitLevel.COMMIT_MASTER;}
+	private TestKVS(){
+		wPolicy.commitLevel=CommitLevel.COMMIT_MASTER;
+	}
 	
 	@Override
 	protected void finalize(){
 		this.client.close();
 	}
 	
-	public void send(){
-		
+	public <T> void fire(long offset,T obj,KeyOf<T> key,BinsOf<T> bins){
+		this.client.put(wPolicy,null,key.of(obj),bins.of(obj,offset));
 	}
 	
 	public static void main(String[] args) throws Exception {
+		OperateRequest r = new OperateRequest();
+		r.amount=100;r.uid=1L;r.message="heihei";r.txID="txid01";
+		TestKVS.getInstance().fire(1,r,keyOfOperateRequest,binsOfOperateRequest);
+		Thread.sleep(5000L);
+	}
+	
+	public static void rawTest() throws Exception{
 		OperateRequest r = new OperateRequest();
 		r.amount=100;r.uid=1L;r.message="heihei";r.txID="txid01";
 		AsyncClient client = new AsyncClient("localhost", 3000);
@@ -42,6 +56,7 @@ public class TestKVS {
 			Bin amt = new Bin("amount",100);
 			Bin bal = new Bin("balance",100);
 			Bin offset = new Bin("offset",1);
+			
 			client.put(policy, 
 					success((donekey)->{
 						System.out.println(donekey.toString()+" created successfully in seperate write callback");
